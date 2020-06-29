dcc44dfe1a25ee0c3951c3eaf2c48e6c166cb478
fenix <thefenix91@gmail.com>
2020-06-30 01:34:38 +0200

    Time: Date was 1 month late.

--- functions/createEmbed.js
+++ functions/createEmbed.js
@@ -32,7 +32,7 @@ function translateDate(timestamp, language) {
 	const UTCtime = UTC[language];
 	const currentTime = new Date(parseInt(timestamp,10) + UTCtime * 3600000);
 	const time = '**' + currentTime.getHours() + 'h' + (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes() + '**';
-	const date = ' - ' + currentTime.getDate() + '/' + currentTime.getMonth() + '/' + currentTime.getFullYear();
+	const date = ' - ' + currentTime.getDate() + '/' + (currentTime.getMonth()+1) + '/' + currentTime.getFullYear();
 	const timeString = time + (language === "en" ? ' (UTC+2)' : '') + date;
 
 	return timeString;
