04cf56eec6b36096a45968bc4db4890774f09178
fenix <thefenix91@gmail.com>
2020-07-01 20:00:40 +0200

    Fix status #4

--- functions/sendStatus.js
+++ functions/sendStatus.js
@@ -2,13 +2,13 @@ const createEmbedCompact = require('../functions/createEmbedCompact.js');
 const Status = require("../schemas/Status.schema.js");
 const Guilds = require("../schemas/Guilds.schema.js");
 
-const sendStatus = (channel, language) => {
+const sendStatus = (channel) => {
 	Guilds.findOne({ guildId: channel.guild.id }).then(res => {
 		if (!res) return;
 		else {
 			Status.findOne({ id: "1" }).then(status => {
-				const statusEmbed = createEmbedCompact(status, language);
-				channel.send(statusEmbed, res.language);
+				const statusEmbed = createEmbedCompact(status, res.language);
+				channel.send(statusEmbed);
 			});
 		}
 	})
