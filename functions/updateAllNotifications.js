5201fc46ff35868362f7f1bb850bbc815ca77b27
fenix <thefenix91@gmail.com>
2021-02-09 15:57:14 +0100

    Update 1.1

--- functions/updateAllNotifications.js
+++ functions/updateAllNotifications.js
@@ -2,11 +2,11 @@ const updateNotifications = require("../functions/updateNotifications.js");
 const Guilds = require('../schemas/Guilds.schema.js');
 const Status = require('../schemas/Status.schema.js');
 
-const updateAllNotifications = (client, bgType, statusType) => {
+const updateAllNotifications = (client, bgType, color) => {
 
 	Status.findOne({ id: "1" }).then(status => {
 		let updateTime;
-		if (statusType === "orange") updateTime = status['last' + bgType];
+		if (color === "orange") updateTime = status['last' + bgType];
 
 		client.guilds.cache.forEach((guild) => {
 			Guilds.findOne({ guildId: guild.id }).then(res => {
@@ -17,7 +17,7 @@ const updateAllNotifications = (client, bgType, statusType) => {
 					if (channels && channels.length > 0) {
 						channels = channels.sort((a, b) => { return a.calculatedPosition - b.calculatedPosition;}); 
 						if (channels[0].permissionsFor(guild.me).has(["SEND_MESSAGES"]))
-							updateNotifications(channels[0], bgType, res.language, updateTime);
+							updateNotifications(channels[0], bgType, res.language, color, updateTime);
 					}
 					else return;
 				}
--- functions/updateNotifications.js
+++ functions/updateNotifications.js
@@ -1,7 +1,7 @@
 const createEmbed = require('../functions/createEmbed.js');
 const bgDescriptors = require('../constants/battleground-descriptors');
 
-const updateNotifications = (channel, bgType, language, updateTime) => {
+const updateNotifications = (channel, bgType, language, color, updateTime) => {
 	let botMessages;
 	channel.messages.fetch({ limit: 100 }).then(messages => {
 		botMessages = messages.filter(msg => msg.author.bot);
@@ -9,7 +9,7 @@ const updateNotifications = (channel, bgType, language, updateTime) => {
 			// Find the good Embed with status recap.
 			for (let i=0; i < botMessages.array().length; i++){
 				if (botMessages.array()[i].embeds[0] && botMessages.array()[i].embeds[0].title === bgDescriptors[language][bgType].title) {
-					const statusEmbed = createEmbed(bgType, 'orange', language, updateTime);
+					const statusEmbed = createEmbed(bgType, color, language, updateTime);
 					botMessages.array()[i].edit(statusEmbed);
 				}
 			}
