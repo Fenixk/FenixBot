1774177a4ef9d5189da33499059f5f19c8dbed44
fenix <thefenix91@gmail.com>
2021-02-11 23:47:15 +0100

    Update sendNotification.js

--- functions/sendNotification.js
+++ functions/sendNotification.js
@@ -24,12 +24,9 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 
 	const announceEmbed = createEmbed(bgType, 'green', language);
 	const announceMessage = "Hello " + roleString + ", **__" + bgType.charAt(0).toUpperCase() + bgType.slice(1) + "__** is currently popping !" + userMessage;
-	console.log("ok0");
 	channel.messages.fetch({ limit: 100 }).then(messages => {
-		console.log("ok1");
 		botMessages = messages.filter(msg => msg.author.bot);
 		if (botMessages.array().length > 0) {
-			console.log("ok2");
 			if (botMessages.array()[i].embeds[0] && botMessages.array()[i].embeds[0].title === bgDescriptors[language][bgType].title) {
 				botMessages.array()[i].edit(announceEmbed)
 				.then(res => console.log('Update Notification for ' + guild))
@@ -44,11 +41,13 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 					botMessages.array()[i].embeds[0].title === statusDescriptors.ru.title || 
 					botMessages.array()[i].embeds[0].title === statusDescriptors.de.title )) {
 					statusExist = true;
+					console.log("ok00");
 				}
 			}
 		}
-
+		console.log("ok0");
 		if (statusExist === false) {
+			console.log("ok1");
 			console.log('Send Status because not exist in ' + guild);
 			Status.findOne({ id: "1" }).then(status => {
 				const statusEmbed = createEmbedCompact(status, language);
@@ -56,6 +55,7 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 			});
 		}
 		if (exist === false) {
+			console.log("ok2");
 			console.log('Send ' + bgType + ' notification to ' + guild);
 			setTimeout(() => channel.send(announceMessage, announceEmbed), 1000);
 		}
