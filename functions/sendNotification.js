a5ee3fabf5a1e89ca440dac76d49bc1667e928e6
fenix <thefenix91@gmail.com>
2021-02-11 23:51:04 +0100

    Update sendNotification.js

--- functions/sendNotification.js
+++ functions/sendNotification.js
@@ -21,10 +21,11 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 	if (userName !== ''){
 		userMessage = ' Thanks ' + userName + ' !';
 	}
-
+	console.log("ok");
 	const announceEmbed = createEmbed(bgType, 'green', language);
 	const announceMessage = "Hello " + roleString + ", **__" + bgType.charAt(0).toUpperCase() + bgType.slice(1) + "__** is currently popping !" + userMessage;
 	channel.messages.fetch({ limit: 100 }).then(messages => {
+		console.log("ok-1");
 		botMessages = messages.filter(msg => msg.author.bot);
 		if (botMessages.array().length > 0) {
 			if (botMessages.array()[i].embeds[0] && botMessages.array()[i].embeds[0].title === bgDescriptors[language][bgType].title) {
@@ -32,7 +33,9 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 				.then(res => console.log('Update Notification for ' + guild))
 				.catch(err => console.log('Failed to edit embed for SEND NOTIFICATION in ' + guild));
 				exist = true;
+				console.log("ok0");
 			}
+			console.log("ok1");
 			// Find the good Embed.
 			for (let i=0; i < botMessages.array().length; i++){
 				if (botMessages.array()[i].embeds[0] && (
@@ -41,13 +44,13 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 					botMessages.array()[i].embeds[0].title === statusDescriptors.ru.title || 
 					botMessages.array()[i].embeds[0].title === statusDescriptors.de.title )) {
 					statusExist = true;
-					console.log("ok00");
+					console.log("ok2");
 				}
 			}
 		}
-		console.log("ok0");
+		console.log("ok3");
 		if (statusExist === false) {
-			console.log("ok1");
+			console.log("ok4");
 			console.log('Send Status because not exist in ' + guild);
 			Status.findOne({ id: "1" }).then(status => {
 				const statusEmbed = createEmbedCompact(status, language);
@@ -55,7 +58,7 @@ const sendNotification = (channel, bgType, guild, language, roleId, userName = '
 			});
 		}
 		if (exist === false) {
-			console.log("ok2");
+			console.log("ok5");
 			console.log('Send ' + bgType + ' notification to ' + guild);
 			setTimeout(() => channel.send(announceMessage, announceEmbed), 1000);
 		}
